0d155b5c6c92e1d44856799c4bde36ce9f8da989
basilicokarol <116791261+basilicokarol@users.noreply.github.com>
2022-12-15 10:29:47 +0100

    Add files via upload
    prima vesrione

---
+++ RnaTranscription.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public static class TrascrizioneRNA
+{
+    public static string ToRna(string dna)
+    {
+        if( String.IsNullOrEmpty( dna ) )
+            return "";
+        string rna = "";
+        for (int i = 0; i < dna.Length; i++)
+        {
+            
+            switch (dna[i])
+            {
+                case 'G':
+                    rna = rna + "C";
+                    break;
+
+                case 'C':
+                    rna = rna + "G";
+                    break;
+
+                case 'T':
+                    rna = rna + "A";
+                    break;
+
+                case 'A':
+                    rna = rna + "U";
+                    break;
+            }
+        }
+
+            return rna;
+    }
+}
